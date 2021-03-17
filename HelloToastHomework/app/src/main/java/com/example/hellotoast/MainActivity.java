ba2ac3e5dfb5ed47219bd4899e6e670ab7451c76
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-17 12:13:14 +0545

    Modified the Toast Buuton to Display Second Activity

--- HelloToastHomework/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToastHomework/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,32 +2,37 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    public static final String EXTRA_MESSAGE
+            = "com.example.android.twoactivities.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
-    }
-
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
+        mShowCount = findViewById(R.id.show_count);
     }
 
     public void countUp(View view) {
-        ++mCount;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(String.format(Locale.ENGLISH, "%d", mCount));
+        }
+    }
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mCount);
+        startActivity(intent);
     }
 }
